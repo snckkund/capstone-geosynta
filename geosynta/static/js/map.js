1aaef7af54221e87c8d0461f28a11dbd86e54fc3
snckkund <sn003chandrakant@gmail.com>
2024-11-30 19:08:47 +0530

    Remove unused files and update .gitignore

--- geosynta/static/js/map.js
+++
@@ -1,5 +0,0 @@
-var map = L.map('map').setView([22.3, 82.13], 5);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
